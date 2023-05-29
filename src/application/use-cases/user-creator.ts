ba2bb0442b256bf62c99b7cc9fb4965a89c2d152
manuelh_ra <manuelhra.dev@gmail.com>
2023-05-29 14:55:22 -0500

    Create alreadyUser exception

--- src/application/use-cases/user-creator.ts
+++ src/application/use-cases/user-creator.ts
@@ -1,11 +1,14 @@
+import { type User } from 'domain/entities/user'
 import { type UserRepository } from 'domain/repositories/user-repository'
+import { type ExistUserByUsername } from 'domain/services/exist-user-by-username'
 
 export class UserCreatorUseCase {
   constructor (
-    private readonly userRepository: UserRepository
+    private readonly userRepository: UserRepository,
+    private readonly existUserByUsername: ExistUserByUsername
   ) {}
 
-  run (): void {
-    console.log('Run')
+  run (body: User): void {
+    const existUser: boolean = this.existUserByUsername.run(body.username)
   }
 }
